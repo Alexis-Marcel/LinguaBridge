7b5f47112b31c0b0b4e48fa620774aed0e6d2d8d
kour0 <lucas.laurent06@gmail.com>
2024-04-24 15:46:43 +0200

    FIX: default values

--- database/migrations/2024_02_28_174500_create_sessions_table.php
+++ database/migrations/2024_02_28_174500_create_sessions_table.php
@@ -14,8 +14,8 @@ return new class extends Migration
         Schema::create('sessions', function (Blueprint $table) {
             $table->id();
             $table->string('session_title');
-            $table->string('meeting_id');
-            $table->string('meeting_password');
+            $table->string('meeting_id')->default(uniqid());
+            $table->string('meeting_password')->default(uniqid());
             $table->string('language1_id', 2);
             $table->string('language2_id', 2);
             $table->foreign('language1_id')->references('code')->on('languages');
